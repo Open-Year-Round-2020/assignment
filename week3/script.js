96f5f4ecb052e4edfa6098c97497658203838b75
Jeonghwa-Lee <64410467+Jeonghwa-Lee@users.noreply.github.com>
2020-05-12 01:42:37 +0900

    week3 assignment _ base part

---
+++ week3/script.js
@@ -0,0 +1,11 @@
+document.getElementById("modaldot").onclick = function() {
+    document.getElementById("modal_popup").style.display="block";
+}
+
+document.getElementById("m_cancel").onclick = function() {
+    document.getElementById("modal_popup").style.display="none";
+}   
+
+document.getElementById("modal_popup").onclick = function() {
+    document.getElementById("modal_popup").style.display="none";
+}   
