03d8567ba4ed9d0a56437f4f9c4e1464277de69d
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-05-15 19:55:16 +0900

    add regexp in login page

--- instagram/scripts/index.js
+++ instagram/scripts/index.js
@@ -5,8 +5,17 @@ const btn = document.querySelector(".login__btn");
 const handleClick = () => {
   const inputId = id.value;
   const inputPw = pw.value;
-  if (inputId === "root" && inputPw === "root") {
-    alert(`id: ${inputId}, pw: ${inputPw}, 로그인 성공!!`);
+
+  let regexp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  if (inputId.length < 6 || !regexp.test(inputId)) {
+    alert("이메일 형식에 맞지 않습니다.");
+    id.select();
+    id.focus();
+    return false;
+  }
+
+  if (inputId === "root@root.com" && inputPw === "root") {
+    alert(`email: ${inputId}, pw: ${inputPw}, 로그인 성공!!`);
     location.href = "./dashboard/index.html";
   } else {
     alert("로그인 실패!!");
