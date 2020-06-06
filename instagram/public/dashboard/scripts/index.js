ece128a469559429f3b3b18ffd180f8e77acfd5b
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-06-07 00:19:19 +0900

    change haert color when user pushed

--- instagram/public/dashboard/scripts/index.js
+++ instagram/public/dashboard/scripts/index.js
@@ -7,6 +7,7 @@ const recommend = document.querySelector(".container__recommend");
 const header = document.querySelector(".header");
 const body = document.querySelector("body");
 const settings = document.querySelector(".settings");
+const hearts = document.querySelectorAll(".fa-heart");
 
 function redirectSettings() {
   location.href = "../settings/index.html";
@@ -39,6 +40,17 @@ function handleResize() {
   header.style.padding = `0 ${container.offsetLeft}px`;
 }
 
+function handleHeartClick(event) {
+  const targetHeart = event.target;
+  if (targetHeart.classList.contains("fas")) {
+    targetHeart.classList.remove("fas");
+    targetHeart.classList.add("far");
+  } else {
+    targetHeart.classList.remove("far");
+    targetHeart.classList.add("fas");
+  }
+}
+
 function init() {
   let offset = container.offsetLeft + 610 + 20;
   recommend.style.left = `${offset}px`;
@@ -50,5 +62,6 @@ init();
 settings.addEventListener("click", redirectSettings);
 window.addEventListener("resize", handleResize);
 dots.forEach((dot) => dot.addEventListener("click", handleClick));
+hearts.forEach((heart) => heart.addEventListener("click", handleHeartClick));
 window.addEventListener("click", handleOutsideClick);
 cancelBtn.addEventListener("click", handleCancelClick);
