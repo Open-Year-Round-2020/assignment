4dacb1c6970b887a59873d450ef9e43e5c86e024
hoho9505 <49706440+hoho9505@users.noreply.github.com>
2020-06-07 23:00:43 +0900

    create new folder

---
+++ Instagram/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var router = express.Router();
+
+app.set('port', 3000);
+
+app.get('/', function(req, res){
+  res.send(path.join(__dirname+"/instagram tasks/index.html"))
+});
