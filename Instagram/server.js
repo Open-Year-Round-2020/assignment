852947faa87577883c414c86894be78d11ce62c1
hoho9505 <49706440+hoho9505@users.noreply.github.com>
2020-06-07 23:09:53 +0900

    Update server.js

--- Instagram/server.js
+++ Instagram/server.js
@@ -6,5 +6,5 @@ var router = express.Router();
 app.set('port', 3000);
 
 app.get('/', function(req, res){
-  res.send(path.join(__dirname+"/instagram tasks/index.html"))
+  res.send(path.join(__dirname+"/instagram_tasks/index.html"))
 });
